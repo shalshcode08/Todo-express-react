dc730ddc2b298fa08ab9502755634ae3d8481ef2
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-10-23 16:38:01 +0530

    Added mongoose schema

---
+++ Backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://admin:somya123sakshi@cluster0.4ssf9r4.mongodb.net/todos")
+
+const todoSchema = mongoose.Schema({
+    title : String(),
+    content : String(),
+    completed : Boolean
+})
+
+const todo = mongoose.model('todo', todoSchema)
+
+module.exports = {
+    todo
+}
