6cea45f189dbfb12bea96713fb8de21d89d63940
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-10-23 16:56:25 +0530

    backend compleye

--- Backend/db.js
+++ Backend/db.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose');
 mongoose.connect("mongodb+srv://admin:somya123sakshi@cluster0.4ssf9r4.mongodb.net/todos")
 
 const todoSchema = mongoose.Schema({
-    title : String(),
-    content : String(),
+    title : {
+        type : String,
+        required : true
+    },
+    content : {
+        type : String,
+        required : true,
+    },
     completed : Boolean
 })
 
